b388720b60071c636b8380ce07a1fadb1a483cd4
kevcb1988 <kevcb1988@gmail.com>
2022-06-27 11:25:29 -0500

    LocalStorage, notifiación cantidad de libros y creación y carga de categorias para filtrar.

--- js/store.js
+++ js/store.js
@@ -5,7 +5,8 @@ const stockBooks = [
         titulo: "Harry Potter y la piedra filosofal",
         autor: 'J.K Rowling',
         precio: '45.000',
-        genero: "Fantasía"
+        genero: "Drama",
+        sinopsis: "Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Pellentesque in ipsum id orci porta dapibus. Donec sollicitudin molestie malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus suscipit tortor eget felis porttitor volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem."
     },
     {
         id: 2,
