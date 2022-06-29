f2608b5bbf284b8c8e6dde34fe2b7a9980ad5b5a
kevcb1988 <kevcb1988@gmail.com>
2022-06-28 22:02:34 -0500

    Correción errores carga de imagenes

--- js/store.js
+++ js/store.js
@@ -1,16 +1,15 @@
 const stockBooks = [
     {
         id: 1,
-        portada: '../img/harry_potter_01.jpeg',
+        portada: './img/harry_potter_01.jpeg',
         titulo: "Harry Potter y la piedra filosofal",
         autor: 'J.K Rowling',
         precio: '45.000',
-        genero: "Fantasía",
-        sinopsis: "Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Pellentesque in ipsum id orci porta dapibus. Donec sollicitudin molestie malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus suscipit tortor eget felis porttitor volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem."
+        genero: "Fantasía"
     },
     {
         id: 2,
-        portada: '../img/harry_potter_02.jpeg',
+        portada: './img/harry_potter_02.jpeg',
         titulo: "Harry Potter y la cámara secreta",
         autor: 'J.K Rowling',
         precio: '45.000',
@@ -18,7 +17,7 @@ const stockBooks = [
     },
     {
         id: 3,
-        portada: '../img/harry_potter_03.jpeg',
+        portada: './img/harry_potter_03.jpeg',
         titulo: "Harry Potter y el prisonero de Azkaban",
         autor: 'J.K Rowling',
         precio: '45.000',
@@ -26,7 +25,7 @@ const stockBooks = [
     },
     {
         id: 4,
-        portada: '../img/harry_potter_04.jpeg',
+        portada: './img/harry_potter_04.jpeg',
         titulo: "Harry Potter y el cáliz de fuego",
         autor: 'J.K Rowling',
         precio: '45.000',
@@ -34,7 +33,7 @@ const stockBooks = [
     },
     {
         id: 5,
-        portada: '../img/harry_potter_05.jpeg',
+        portada: './img/harry_potter_05.jpeg',
         titulo: "Harry Potter y la Orden del Fénix",
         autor: 'J.K Rowling',
         precio: '45.000',
@@ -42,7 +41,7 @@ const stockBooks = [
     },
     {
         id: 6,
-        portada: '../img/harry_potter_06.jpeg',
+        portada: './img/harry_potter_06.jpeg',
         titulo: "Harry Potter y el misterio del principe",
         autor: 'J.K Rowling',
         precio: '45.000',
@@ -58,7 +57,7 @@ const stockBooks = [
     },
     {
         id: 8,
-        portada: '../img/el_marciano.jpg',
+        portada: './img/el_marciano.jpg',
         titulo: "El marciano",
         autor: 'Andy Weir',
         precio: '45.000',
@@ -66,7 +65,7 @@ const stockBooks = [
     },
     {
         id: 9,
-        portada: '../img/aquiles.jpg',
+        portada: './img/aquiles.jpg',
         titulo: "La Canción de Aquiles",
         autor: 'Madeleine Miller',
         precio: '45.000',
@@ -74,7 +73,7 @@ const stockBooks = [
     },
     {
         id: 10,
-        portada: '../img/bellas_durmientes.jpg',
+        portada: './img/bellas_durmientes.jpg',
         titulo: "Bellas Durmientes",
         autor: 'Stephen King',
         precio: '45.000',
@@ -82,7 +81,7 @@ const stockBooks = [
     },
     {
         id: 11,
-        portada: '../img/el_club_5_am.jpg',
+        portada: './img/el_club_5_am.jpg',
         titulo: "El Club de las 5 de la mañana",
         autor: 'Robin Sharma',
         precio: '45.000',
@@ -90,7 +89,7 @@ const stockBooks = [
     },
     {
         id: 12,
-        portada: '../img/estoicismo_cotidiano.jpg',
+        portada: './img/estoicismo_cotidiano.jpg',
         titulo: "Estoicismo Cotidiano",
         autor: 'Ryan Holiday / Stephen Hanselman',
         precio: '45.000',
